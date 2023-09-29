e4f3a89db2fb20f21b0722e9fad4abc6a522ba52
Christian Georgeot <christian.georgeot@gmail.com>
2023-09-29 19:28:01 +0200

    Mise en place d'une palette de couleur Planty

--- wp-content/themes/twentytwenty-child/widgets/planty-gout-widget.php
+++ wp-content/themes/twentytwenty-child/widgets/planty-gout-widget.php
@@ -6,19 +6,19 @@ class Planty_Gout_Widget extends WP_Widget {
         parent::__construct(
             'planty_gout_widget',
             'Widget Gout Fruit',
-            array('description' => __( 'Widget décrivant un parfum fruité', 'text_domain'), )
-        );
+            array('description' => __('Widget décrivant un parfum fruité', 'text_domain'))
+       );
     }
 
-    public function widget( $args, $instance ) {
-        extract( $args );
+    public function widget($args, $instance) {
+        extract($args);
         $title = $instance['title'];
         $textSize = $instance['text_size'];
         $urlImage = $instance['url_image'];
 
         echo $before_widget;
 
-        if ( !empty( $title ) && !empty( $textSize ) && !empty( $urlImage ) ) {
+        if (!empty($title) && !empty($textSize) && !empty($urlImage)) {
             ?>
             <div class="planty-gout-widget">
                 <img 
@@ -37,18 +37,18 @@ class Planty_Gout_Widget extends WP_Widget {
         echo $after_widget;
     }
 
-    public function form( $instance ) {
-        if ( isset( $instance[ 'title' ] ) ) {
+    public function form($instance) {
+        if (isset($instance[ 'title' ])) {
             $title = $instance[ 'title' ];
         } else {
             $title = "Insipide";
         }
-        if ( isset( $instance[ 'text_size' ] ) ) {
+        if (isset($instance[ 'text_size' ])) {
             $textSize = $instance[ 'text_size' ];
         } else {
             $textSize = "20";
         }
-        if ( isset( $instance[ 'url_image' ] ) ) {
+        if (isset($instance[ 'url_image' ])) {
             $urlImage = $instance[ 'url_image' ];
         } else {
             $urlImage = "";
@@ -56,49 +56,49 @@ class Planty_Gout_Widget extends WP_Widget {
         ?>
         <p>
             <label 
-                for="<?= $this->get_field_name( 'title' ); ?>">
+                for="<?= $this->get_field_name('title'); ?>">
                 Nom du goût
             </label>
             <input class="widefat" 
-                id="<?= $this->get_field_id( 'title' ); ?>" 
-                name="<?= $this->get_field_name( 'title' ); ?>" 
+                id="<?= $this->get_field_id('title'); ?>" 
+                name="<?= $this->get_field_name('title'); ?>" 
                 type="text" 
-                value="<?= esc_attr( $title ); ?>"
+                value="<?= esc_attr($title); ?>"
             >
         </p>
         <p>
             <label 
-                for="<?= $this->get_field_name( 'text_size' ); ?>">
+                for="<?= $this->get_field_name('text_size'); ?>">
                 Taille du texte
             </label>
             <input class="widefat" 
-                id="<?= $this->get_field_id( 'text_size' ); ?>" 
-                name="<?= $this->get_field_name( 'text_size' ); ?>" 
+                id="<?= $this->get_field_id('text_size'); ?>" 
+                name="<?= $this->get_field_name('text_size'); ?>" 
                 type="text"
-                value="<?= esc_attr( $textSize ); ?>"
+                value="<?= esc_attr($textSize); ?>"
             >
         </p>
         <p>
             <label 
-                for="<?= $this->get_field_name( 'url_image' ); ?>">
+                for="<?= $this->get_field_name('url_image'); ?>">
                 Url de l'image
             </label>
             <input class="widefat" 
-                id="<?= $this->get_field_id( 'url_image' ); ?>" 
-                name="<?= $this->get_field_name( 'url_image' ); ?>"
+                id="<?= $this->get_field_id('url_image'); ?>" 
+                name="<?= $this->get_field_name('url_image'); ?>"
                 type="text"
-                value="<?= esc_attr( $urlImage ); ?>"
+                value="<?= esc_attr($urlImage); ?>"
             >
         </p>
         <?php
     }
 
-    public function update( $new_instance, $old_instance) {
+    public function update($new_instance, $old_instance) {
         $instance = array();
 
-        $instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-        $instance['text_size'] = ( !empty( $new_instance['text_size'] ) ) ? strip_tags( $new_instance['text_size'] ) : '';
-        $instance['url_image'] = ( !empty( $new_instance['url_image'] ) ) ? strip_tags( $new_instance['url_image'] ) : '';
+        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        $instance['text_size'] = (!empty($new_instance['text_size'])) ? strip_tags($new_instance['text_size']) : '';
+        $instance['url_image'] = (!empty($new_instance['url_image'])) ? strip_tags($new_instance['url_image']) : '';
 
         return $instance;
     }

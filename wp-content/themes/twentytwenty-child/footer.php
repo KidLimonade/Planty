1ec44ebe92f14d2ff2760dd2df39c02f68fd47d3
Christian Georgeot <christian.georgeot@gmail.com>
2023-10-02 06:28:06 +0200

    footer indentation et coquille

--- wp-content/themes/twentytwenty-child/footer.php
+++ wp-content/themes/twentytwenty-child/footer.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * Planty code for displaying the footer
- *
- * Contains the opening of the #site-footer div and all content after.
- */
+* Planty code displaying the footer
+*
+* Contains the opening of the #site-footer div and all content after.
+*/
 
 ?>
-			<footer id="site-footer" class="header-footer-group">
+<footer id="site-footer" class="header-footer-group">
+    
+    <div class="footer-inner">
+        
+        <?php
+        if ( function_exists( 'the_privacy_policy_link' ) ) {
+            the_privacy_policy_link( '<p class="privacy-policy">', '</p>' );
+        } else {
+            ?>
+            <p class="privacy-policy">
+                <a class="privacy-policy-link" href="#">Mentions légales</a>
+            </p>
+            <?php
+        }
+        ?>
+        
+    </div><!-- .footer-inner -->
+    
+</footer><!-- #site-footer -->
 
-				<div class="footer-inner">
+<?php wp_footer(); ?>
 
-					<?php
-					if ( function_exists( 'the_privacy_policy_link' ) ) {
-						the_privacy_policy_link( '<p class="privacy-policy">', '</p>' );
-					} else {
-                        ?>
-                        <p class="privacy-policy">
-                            <a class="privacy-policy-link" href="#">Mention légales</a>
-                        </p>
-                    <?php
-                    }
-					?>
-
-				</div><!-- .footer-inner -->
-
-			</footer><!-- #site-footer -->
-
-		<?php wp_footer(); ?>
-
-	</body>
+</body>
 </html>
